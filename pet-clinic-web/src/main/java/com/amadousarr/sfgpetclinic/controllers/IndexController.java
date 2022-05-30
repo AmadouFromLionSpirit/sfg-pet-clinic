d059b7b30c0f6cfd83d290d95518284f190d18f1
Sarr <contact@amadousarr.com>
2022-05-30 17:47:24 +0200

    Fixed Broken Links. closes #45

--- pet-clinic-web/src/main/java/com/amadousarr/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/amadousarr/sfgpetclinic/controllers/IndexController.java
@@ -11,4 +11,11 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
+    }
+
+
 }
