1e216fee0f5692951d8c61780b689942ac72d3fa
Sarr <contact@amadousarr.com>
2022-06-01 16:10:52 +0200

    Update base entity with JPA annotation. closes #35

--- pet-clinic-data/src/main/java/com/amadousarr/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/amadousarr/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,19 @@
 package com.amadousarr.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+/*@MappedSuperclass is saying hey JPA, we're going to inherit from this class or other classes are going inherit it
+* we don't need this specific class mapped to the database. We are doing inheritance but don't worry about doing anything
+*  special for it
+* */
+@MappedSuperclass
 
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
