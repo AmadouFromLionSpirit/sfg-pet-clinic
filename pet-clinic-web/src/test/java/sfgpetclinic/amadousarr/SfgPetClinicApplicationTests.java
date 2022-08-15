218eab6068c546ef27fbf43de95d1173f81f7ca9
Sarr <contact@amadousarr.com>
2022-08-15 18:40:56 +0200

    Updated to Junit 5

--- pet-clinic-web/src/test/java/sfgpetclinic/amadousarr/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/amadousarr/SfgPetClinicApplicationTests.java
@@ -1,11 +1,15 @@
 package sfgpetclinic.amadousarr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ContextConfiguration
-class SfgPetClinicApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@SpringBootConfiguration
+public class SfgPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
