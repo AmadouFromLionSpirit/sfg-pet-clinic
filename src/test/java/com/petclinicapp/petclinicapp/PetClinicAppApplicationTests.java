1a1b5a6501de9ff156ad92cd476086c32870d2e2
AMADOUSARR <amadou.sarr@bdl.lu>
2023-09-19 15:29:00 +0200

    Initial commit

---
+++ src/test/java/com/petclinicapp/petclinicapp/PetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinicapp.petclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
